a72feb3ab088caea09c5258140b4555d11510a26
leonardogonzalolaura <glleonardodaniel@gmail.com>
2025-07-27 20:14:43 -0500

    update auth

--- routes.js
+++ routes.js
@@ -6,8 +6,8 @@ const { swaggerServe, swaggerSetup } = require('./middleware/swagger_doc');
 
 module.exports = function (app, opts) {
   // Setup routes, middleware, and handlers
+  app.use('/api/swaggerUI', swaggerServe , swaggerSetup );
   app.use(authMiddleware);
   app.post('/api/login', security)
-  app.use('/api/swaggerUI', swaggerServe , swaggerSetup );
   app.get('/configured', configured(opts))
 }
